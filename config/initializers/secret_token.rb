4df46b3cf15a68c98092d2b0420f3613287c5a93
David Vezzani <david_vezzani@yahoo.com>
2013-10-23 06:36:47 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HairyHappiness::Application.config.secret_key_base = '3ba130f8b121f43ea54ebf66b4c02367e77db55bab41f27e950f5896b4edd27ea21b853fc294dc93b5aa76c28b1c38e487c053818d2d1a734ffb7d06b4c3cfbe'
